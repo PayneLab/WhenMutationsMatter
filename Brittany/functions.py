c1bfcad80191b25d79b0eee667c8abd927ca62a6
blhmc1 <bhendmore2@gmail.com>
2019-07-16 15:40:55 -0600

    create functions.py and debug get_freq_mut

---
+++ Brittany/functions.py
@@ -0,0 +1,104 @@
+def format_cis_comparison_data(cancer_object, specific_omics, gene):
+    
+    # Step 1 - Create dataframe in order to do comparisons with wrap_ttest - drop nan values
+    omics_and_mutations = cancer_object.append_mutations_to_omics(
+        mutation_genes = gene, omics_df_name = specific_omics.name, omics_genes = gene).dropna()
+    print(omics_and_mutations.head())
+    # Check if values in omics data (if not found in proteomics, after na dropped dataframe should be empty)
+    if omics_and_mutations[gene+"_"+specific_omics.name].empty:
+        print('Not possible to do T-test. No data for', gene, 'in', specific_omics.name)
+        return None
+    else:
+        
+        # Step 2 - Create the binary column needed to do the comparison
+        omics_and_mutations['binary_mutations'] = np.where(
+            omics_and_mutations[gene+'_Mutation_Status'] == 'Wildtype_Tumor', 'Wildtype', 'Mutated')
+
+        # Step 3 - Format the dataframe correctly for the T-test(just omics and binary columns for tumors)
+        tumors = omics_and_mutations.loc[omics_and_mutations['Sample_Status'] == 'Tumor'] #drop Normal samples
+        columns_to_drop = [gene+"_Mutation", gene+"_Location", gene+"_Mutation_Status", "Sample_Status"]
+        omics_binary_mutations = tumors.drop(columns_to_drop, axis = 1)
+        #check if only one column of omics data (total 2 columns)
+        if len(omics_binary_mutations.columns) != 2:
+            print('exeption with columns. check omics data')
+            return None
+        else:
+            # Make a list of the column label of omics to be used in the wrap_ttest function
+            omics_col_list = [omics_binary_mutations.columns[0]] 
+
+            # Step 4 - T-test comparing means of mutated vs wildtype effect on cis omics
+            print("Doing t-test comparison\n")
+            significant_pval_results = al.wrap_ttest(omics_binary_mutations, 'binary_mutations', omics_col_list)
+            print(significant_pval_results)
+            
+            
+
+            formated_data_for_boxplot = {'data': omics_binary_mutations, 'x': "binary_mutations", 
+                                         'y': gene+"_proteomics", 'pval': significant_pval_results}
+            return formated_data_for_boxplot
+
+def get_missence_truncation_comparison(cancer_object, specific_omics, gene):
+    specific_omics = cancer_object.get_proteomics()
+    #get omics data and tumors
+    omics_and_mutations = cancer_object.append_mutations_to_omics(
+                mutation_genes = gene, omics_df_name = specific_omics.name, omics_genes = gene)
+    tumors = omics_and_mutations.loc[omics_and_mutations['Sample_Status'] == 'Tumor']
+
+
+    somatic_mutations = cancer_object.get_mutations().reset_index()
+
+    if cancer_object.get_cancer_type() == 'colon':
+        missence_truncation_groups = {'frameshift substitution': 'Truncation', 
+            'frameshift deletion': 'Truncation', 'frameshift insertion': 'Truncation', 
+            'stopgain': 'Truncation', 'stoploss': 'Truncation', 'nonsynonymous SNV': 'Missence',
+            'nonframeshift insertion': 'Missence','nonframeshift deletion': 'Missence', 
+            'nonframeshift substitution': 'Missence'}
+    else: 
+        missence_truncation_groups = {'In_Frame_Del': 'Missence', 'In_Frame_Ins': 'Missence',
+            'Missense_Mutation': 'Missence', 'Frame_Shift_Del': 'Truncation','Nonsense_Mutation': 'Truncation', 
+            'Splice_Site': 'Truncation', 'Frame_Shift_Ins': 'Truncation','Nonstop_Mutation':'Truncation'}
+
+    mutations_replaced_M_T = somatic_mutations.replace(missence_truncation_groups)
+    mutations_replaced_M_T = mutations_replaced_M_T.loc[mutations_replaced_M_T['Gene'] == gene]
+
+    # group mutation categories
+    miss = mutations_replaced_M_T.loc[mutations_replaced_M_T['Mutation'] == 'Missence']
+    trunc = mutations_replaced_M_T.loc[mutations_replaced_M_T['Mutation'] == 'Truncation']
+
+    #get lists of unique samples for missence and trucation categories
+    miss_unique_samples = list(miss['Sample_ID'].unique())
+    trunc_unique_samples = list(trunc['Sample_ID'].unique())
+    
+    #check if there is only one type of mutation for the specific gene
+    if miss_unique_samples == []:
+        print('Only truncation type mutations found for', gene+'.', 
+             'Not possible to compare mutation types.')
+        return None
+    elif trunc_unique_samples == []:
+        print('Only missence type mutations found for', gene+'.', 
+             'Not possible to compare mutation types.')
+        return None
+
+    # Step 2 - Create the binary column needed to do the comparison
+    # Get mutation catagories with omics data
+    missence_omics = tumors.loc[tumors.index.isin(miss_unique_samples)]
+    missence_omics = missence_omics.assign(binary_mutations = 'Missence')
+    truncation_omics = tumors.loc[tumors.index.isin(trunc_unique_samples)]
+    truncation_omics = truncation_omics.assign(binary_mutations = 'Truncation')
+    binary_mut_omics = missence_omics.append(truncation_omics)
+
+    # Step 3 - Format the dataframe correctly for the T-test(just omics and binary columns for tumors)
+    columns_to_drop = [gene+"_Mutation", gene+"_Location", gene+"_Mutation_Status", "Sample_Status"]
+    binary_mut_omics = binary_mut_omics.drop(columns_to_drop, axis = 1)
+
+    # Make a list of the column label of omics to be used in the wrap_ttest function
+    omics_col_list = [binary_mut_omics.columns[0]] 
+
+    # Step 4 - T-test comparing means of mutated vs wildtype effect on cis omics
+    print("Doing t-test comparison\n")
+    significant_pval_results = al.wrap_ttest(binary_mut_omics, 'binary_mutations', omics_col_list)
+    print(significant_pval_results)
+
+    formated_data_for_boxplot = {'data': binary_mut_omics, 'x': "binary_mutations", 
+                                 'y': gene+"_"+specific_omics.name, 'pval': significant_pval_results}
+    return formated_data_for_boxplot
