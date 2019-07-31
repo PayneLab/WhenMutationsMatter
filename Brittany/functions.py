a6a3a45c31abf8caf777fc441369899bfc9530b3
blhmc1 <bhendmore2@gmail.com>
2019-07-31 13:02:00 -0600

    update

--- Brittany/functions.py
+++ Brittany/functions.py
@@ -1,4 +1,4 @@
-def format_cis_comparison_data(cancer_object, specific_omics, omics_name, gene):
+def format_cis_comparison_data(cancer_object, omics_name, gene):
     import numpy as np
     # Step 1 - Create dataframe in order to do comparisons with wrap_ttest - drop nan values
     omics_and_mutations = cancer_object.join_omics_to_mutations(
@@ -6,7 +6,7 @@ def format_cis_comparison_data(cancer_object, specific_omics, omics_name, gene):
     #print(omics_and_mutations.head())
     # Check if values in omics data (if not found in proteomics, after na dropped dataframe should be empty)
     if omics_and_mutations[gene+"_"+omics_name].empty:
-        print('Not possible to do T-test. No data for', gene, 'in', specific_omics.name)
+        print('Not possible to do T-test. No data for', gene, 'in', omics_name)
         return None
     else:
         
@@ -26,7 +26,7 @@ def format_cis_comparison_data(cancer_object, specific_omics, omics_name, gene):
         else:
             return omics_binary_mutations
 
-def get_missence_truncation_comparison(cancer_object, specific_omics, omics_name, gene):
+def get_missence_truncation_comparison(cancer_object, omics_name, gene):
     import numpy as np
     #get omics data and tumors
     omics_and_mutations = cancer_object.join_omics_to_mutations(
@@ -89,3 +89,89 @@ def get_missence_truncation_comparison(cancer_object, specific_omics, omics_name
     binary_mut_omics = binary_mut_omics.drop(columns_to_drop, axis = 1)
 
     return binary_mut_omics
+
+
+# PHOSPHOPROTEOMICS
+
+def format_phospho_cis_comparison(cancer_object, omics_name, gene, specific_phospho):
+    import numpy as np
+    # Step 1 - Create dataframe in order to do comparisons with wrap_ttest
+    omics_and_mut = cancer_object.join_omics_to_mutations(
+        mutations_genes = gene, omics_df_name = omics_name, omics_genes = gene)
+
+    # Step 2 - Create the binary column needed to do the comparison
+    omics_and_mut['binary_mutations'] = omics_and_mut[gene+'_Mutation_Status'].apply(
+        lambda x: 'Wildtype' if x == 'Wildtype_Tumor' else 'Mutated')
+
+    # Step 3 - Format the dataframe correctly for the T-test(just omics and binary columns for tumors)
+    tumors = omics_and_mut.loc[omics_and_mut['Sample_Status'] == 'Tumor'] #drop Normal samples
+    binary_phospho = tumors[[specific_phospho, 'binary_mutations']].dropna(axis = 0)
+
+    return binary_phospho
+
+
+def get_missence_truncation_phospho(cancer_object, omics_name, gene, specific_phospho):
+    import numpy as np
+    #get omics data and tumors
+    omics_and_mutations = cancer_object.join_omics_to_mutations(
+                mutations_genes = gene, omics_df_name = omics_name, omics_genes = gene)
+    tumors = omics_and_mutations.loc[omics_and_mutations['Sample_Status'] == 'Tumor'] #drop Normal samples
+
+    somatic_mutations = cancer_object.get_mutations().reset_index()
+
+    if cancer_object.get_cancer_type() == 'colon':
+        missence_truncation_groups = {'frameshift substitution': 'Truncation', 
+            'frameshift deletion': 'Truncation', 'frameshift insertion': 'Truncation', 
+            'stopgain': 'Truncation', 'stoploss': 'Truncation', 'nonsynonymous SNV': 'Missence',
+            'nonframeshift insertion': 'Missence','nonframeshift deletion': 'Missence', 
+            'nonframeshift substitution': 'Missence'}
+    else: 
+        missence_truncation_groups = {'In_Frame_Del': 'Missence', 'In_Frame_Ins': 'Missence',
+            'Missense_Mutation': 'Missence', 'Frame_Shift_Del': 'Truncation','Nonsense_Mutation': 'Truncation', 
+            'Splice_Site': 'Truncation', 'Frame_Shift_Ins': 'Truncation','Nonstop_Mutation':'Truncation'}
+
+    mutations_replaced_M_T = somatic_mutations.replace(missence_truncation_groups)
+    mutations_replaced_M_T = mutations_replaced_M_T.loc[mutations_replaced_M_T['Gene'] == gene]
+
+    # group mutation categories
+    miss = mutations_replaced_M_T.loc[mutations_replaced_M_T['Mutation'] == 'Missence']
+    trunc = mutations_replaced_M_T.loc[mutations_replaced_M_T['Mutation'] == 'Truncation']
+
+    #get lists of unique samples for missence and trucation categories
+    miss_unique_samples = list(miss['Sample_ID'].unique())
+    trunc_unique_samples = list(trunc['Sample_ID'].unique())
+    
+    #check if there is only one type of mutation for the specific gene
+    if miss_unique_samples == []:
+        print('Only truncation type mutations found for', gene+'.', 
+             'Not possible to compare mutation types.')
+        truncation_omics = tumors.loc[tumors.index.isin(trunc_unique_samples)]
+        truncation_omics = truncation_omics.assign(binary_mutations = 'Truncation')
+        columns_to_drop = [gene+"_Mutation", gene+"_Location", gene+"_Mutation_Status", "Sample_Status"]
+        binary_mut_omics = truncation_omics.drop(columns_to_drop, axis = 1)
+        return binary_mut_omics
+    elif trunc_unique_samples == []:
+        print('Only missence type mutations found for', gene+'.', 
+             'Not possible to compare mutation types.')
+        missence_omics = tumors.loc[tumors.index.isin(miss_unique_samples)]
+        missence_omics = missence_omics.assign(binary_mutations = 'Missence')
+        columns_to_drop = [gene+"_Mutation", gene+"_Location", gene+"_Mutation_Status", "Sample_Status"]
+        binary_mut_omics = missence_omics.drop(columns_to_drop, axis = 1)
+        return binary_mut_omics
+
+    # Step 2 - Create the binary column needed to do the comparison
+    # Get mutation catagories with omics data
+    missence_omics = tumors.loc[tumors.index.isin(miss_unique_samples)]
+    missence_omics = missence_omics.assign(binary_mutations = 'Missence')
+    truncation_omics = tumors.loc[tumors.index.isin(trunc_unique_samples)]
+    truncation_omics = truncation_omics.assign(binary_mutations = 'Truncation')
+    binary_mut_omics = missence_omics.append(truncation_omics)
+
+    # Step 3 - Format the dataframe correctly for the T-test(just omics and binary columns for tumors)
+    columns_to_drop = [gene+"_Mutation", gene+"_Location", gene+"_Mutation_Status", "Sample_Status"]
+    binary_mut_omics = binary_mut_omics.drop(columns_to_drop, axis = 1)
+    
+    # select specific col and drop nan rows
+    binary_phospho = binary_mut_omics[[specific_phospho, 'binary_mutations']].dropna(axis = 0)
+
+    return binary_phospho
